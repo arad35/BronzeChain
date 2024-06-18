002d8cbed4c80a5f4a95b50b1291cd3efc7607ed
arad35 <130201644+arad35@users.noreply.github.com>
2024-06-18 13:26:04 +0300

    final blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -14,6 +14,7 @@ import json
 from typing import List
 import traceback
 import time
+import ast
 import sqlite3 as lite
 from blockchain_classes import *
 
@@ -22,22 +23,34 @@ class PacketHandlingError(Exception):
     pass
 
 
+def address_string_to_tuple(address_string):
+    """Convert an address string of the format 'address:port' to a tuple (address, port)"""
+    address, port = address_string.split('_')
+    return address, int(port)
+
+
+def address_tuple_to_string(address_tuple):
+    """Convert a tuple (address, port) to an address string of the format 'address:port'"""
+    return f"{address_tuple[0]}_{address_tuple[1]}"
+
+
 class Peer:
     """Represents a peer in the blockchain network"""
 
-    def __init__(self, name, peers, connect_port, balance=50, miner=False, hop_max=3, time_to_live=240,
+    def __init__(self, name, server_address, balance=50, miner=False, hop_max=3, time_to_live=240,
                  min_transactions=3, max_transactions=5, difficulty=2, reward=5):
         # initialize networking properties
-        self.ip = "localhost"
-        self.packet_cache = dict() # packet hash: time added
-
-        # properties for sending
-        self.send_sock = socket(AF_INET, SOCK_STREAM)
-        self.peers = peers
-
-        # properties for receiving
-        self.connect_port = connect_port
+        ip = gethostbyname(gethostname())
         self.connect_sock = socket(AF_INET, SOCK_STREAM)
+        self.connect_sock.bind((ip, 0))
+        self.address = self.connect_sock.getsockname()
+
+        self.server_address = server_address
+        self.server_sock = socket(AF_INET, SOCK_STREAM)
+
+        self.send_sock = socket(AF_INET, SOCK_STREAM)
+        # cache containing recent packet (hash: timestamp format)
+        self.packet_cache = dict()
 
         # net constants
         self.hop_max = hop_max
@@ -80,7 +93,7 @@ class Peer:
         try:
             conn = lite.connect(self.database_path, check_same_thread=False)
         except lite.Error as e:
-            print(f"error creating peer datbase: {e}")
+            print(f"error creating peer database: {e}")
             return
 
         self.peer_database = conn
@@ -103,7 +116,7 @@ class Peer:
 
         # convert address and public key to string
         public_key = binascii.hexlify(public_key.export_key()).decode()
-        address = str(address)
+        address = address_tuple_to_string(address)
 
         try:
             # check if peer already exists
@@ -113,7 +126,7 @@ class Peer:
 
             # if exists, update row
             if row:
-                cursor.execute("UPDATE Peers SET address = ?, name = ?, public_key = ?, WHERE id = ?",
+                cursor.execute("UPDATE Peers SET address = ?, name = ?, public_key = ? WHERE id = ?",
                                (address, name, public_key, row[0]))
 
             # else insert a new row
@@ -156,6 +169,16 @@ class Peer:
 
         # return false if not found
         return False
+
+    def peers_list(self):
+        """returns list of peer adresses in database"""
+        cursor = self.peer_database.cursor()
+        cursor.execute("SELECT address from Peers WHERE public_key !=?", (self.identity, ))
+        rows = cursor.fetchall()
+
+        peers = [address_string_to_tuple(row[0]) for row in rows]
+        print(f"peers: {peers}")
+        return peers
 
     def compute_packet_hash(self, data):
         """compute hash of a packet accordingly to its type
@@ -244,6 +267,141 @@ class Peer:
         self.verified_transactions.append(transaction)
         print("\nnew transaction added to verified transactions:\n", transaction)
 
+    def send_to_server(self, data, data_type):
+        """send data to server
+        returns: True if data sent successfully, error message otherwise"""
+
+        dict_data = json.loads(data)
+        dict_data["type"] = data_type
+        data = json.dumps(dict_data)
+        try:
+            self.server_sock.sendall(data.encode())
+            print(f"\nsent {data_type} packet to server{self.server_address}:\n{data}")
+
+        except OSError as e:
+            print(f"error when sending {data_type} to server{self.server_address}:\n{e}")
+            return f"error when sending {data_type} to server{self.server_address}:\n{e}"
+
+        return True
+
+    def receive_from_server(self):
+        """receive data from server
+        returns: True if successfully received and processed server_hello message,
+        error message otherwise"""
+        sock = self.server_sock
+        result = False
+        try:
+            # receive data
+            try:
+                json_data = sock.recv(1024).decode()
+                print(f"received from server {self.server_address}:\n{json_data}")
+
+            # socket error (imported socket as *)
+            except error or OSError as e:
+                print(f"\nsocket error while receiving data from server {self.server_address}\n{e}")
+                raise PacketHandlingError
+
+            # handle empty string
+            if json_data == "":
+                print(f"\nempty data from server {self.server_address}")
+                raise PacketHandlingError
+
+            # load data
+            try:
+                data = json.loads(json_data)
+
+            except Exception as e:
+                raise PacketHandlingError(sock, f"data wasn't a valid json\n{e}", json_data)
+
+            try:
+                # process message
+                if data["type"] == "Server_Hello":
+                    # process hello message
+                    result = self.handle_server_hello(json_data)
+                    if isinstance(result, str):
+                        raise PacketHandlingError(sock, result, json_data)
+
+                elif data["type"] == "Message":
+                    # return error message
+                    return data["message"]
+                    pass
+
+                else:
+                    raise PacketHandlingError(sock, "json data of unfamiliar type", json_data)
+
+            except KeyError as e:
+                raise PacketHandlingError(sock, f"data was not a valid Server_Hello message\n{e}", data)
+
+        except PacketHandlingError as e:
+            # unsuccessful processing
+            if len(e.args) == 3:
+                # notify error if needed
+                self.notify_error(*e.args, server=True)
+
+        finally:
+            sock.close()
+            if result is True:
+                return True
+
+    def handle_server_hello(self, hello_packet):
+        """handles a server_hello packet
+        hello_packet: hello packet (json string)
+        returns: True if hello packet processed successfully, error message otherwise"""
+        try:
+            # load data
+            data = json.loads(hello_packet)
+            if not data["sub_type"] == "Response":
+                return "server_hello packet wasn't a response"
+            peer_data = data["peer"]
+
+            if data["peer"] is None:
+                # no peer to update
+                return True
+
+            address = address_string_to_tuple(peer_data["address"])
+            name = peer_data["name"]
+            public_key = RSA.import_key(binascii.unhexlify(peer_data["public_key"]))
+
+        except Exception as e:
+            # possible errors: data doesn't fit hello packet format / missing properties
+            # / data doesn't fit type (e.g. RSAkey)
+            return f"data was not a valid hello packet\n{e}"
+
+        # update peer database
+        self.update_peer(address, name, public_key)
+        return True
+
+    def connect_to_server(self):
+        """connect and interact with server
+        returns: True if connected to server successfully, error message otherwise"""
+        sock = self.server_sock
+        # craft server_hello packet
+        dict_data = {"sub_type": "Request",
+                     "peer": {"address": address_tuple_to_string(self.address), "name": self.name, "public_key": self.identity}}
+        data = json.dumps(dict_data)
+
+        try:
+            sock.connect(self.server_address)
+
+        except OSError as e:
+            print(f"error when sending server_hello to {self.server_address}:\n{e}")
+            return f"error when sending server_hello to {self.server_address}:\n{e}"
+
+        # send server_hello request
+        # result - True if processed successfully, error message else
+        result1 = self.send_to_server(data, data_type="Server_Hello")
+        if isinstance(result1, str):
+            return result1
+
+        # process server_hello response
+        # result - True if processed successfully, error message else
+        result2 = self.receive_from_server()
+        if isinstance(result2, str):
+            return result2
+
+        elif result1 is True and result2 is True:
+            return True
+
     def send_data(self, json_data, new_data=False, data_type=None):
         """sends data to all peers in network
         adds type and hop count properties to packet
@@ -273,7 +431,7 @@ class Peer:
 
         data = json.dumps(dict_data)
         # send transaction to all peers
-        for addr in self.peers:
+        for addr in self.peers_list():
             try:
                 print(addr)
                 time.sleep(2)
@@ -314,26 +472,31 @@ class Peer:
         send_me_hello: whether peers should send back hello (peer discovery)"""
 
         # crafting hello packet
-        data = json.dumps({'address': (self.ip, self.connect_port),
+        data = json.dumps({'address': address_tuple_to_string(self.address),
                            "name": self.name,
                            "public_key": self.identity,
                            "send_me_hello": send_me_hello})
 
         self.send_data(data, new_data=True, data_type="Hello")
 
-    def notify_error(self, sock, message, data):
+    def notify_error(self, sock, message, data, server=False):
         """print and sends a new error message
         message: error message
-        data: string data received from peer"""
+        data: string data received from peer
+        server: whether the recipient is server"""
         print(f"\nerror from {sock.getpeername()}:\n{message}\n{data}")
 
         data_with_message = json.dumps({"message": message, "data": data})
-        self.send_data(data_with_message, new_data=True, data_type="Message")
+        if server:
+            # send error message to server
+            self.send_to_server(data_with_message, data_type="Message")
+        else:
+            # send error message to all peers
+            self.send_data(data_with_message, new_data=True, data_type="Message")
 
     def receive_data(self):
         """thread for connecting and receiving data from other peers"""
         sock = self.connect_sock
-        sock.bind((self.ip, self.connect_port))
         sock.listen(5)
 
         while True:
@@ -489,12 +652,12 @@ class Peer:
 
     def handle_hello(self, hello_packet):
         """handles an hello packet
-        transaction_json: json data with the transaction
+        hello_packet: hello packet (json string)
         returns: True if hello packet processed successfully, error message otherwise"""
         try:
             # load data
             data = json.loads(hello_packet)
-            address = tuple(data["address"])
+            address = address_string_to_tuple(data["address"])
             name = data["name"]
             public_key = RSA.import_key(binascii.unhexlify(data["public_key"]))
 
@@ -594,26 +757,38 @@ class Peer:
             time.sleep(10)
 
     def start(self):
-        """start BronzeChain"""
+        """start BronzeChain
+        returns: True if started the peer successfully, error message otherwise"""
+
+        # initialize database
         self.create_peer_database()
-        self.update_peer((self.ip, self.connect_port), self.name, self.public_key, is_user=True)
+        self.update_peer(self.address, self.name, self.public_key, is_user=True)
 
-        print("I'm here")
-        send_hello_thread = threading.Thread(target=self.send_hello, args=(True, ))
-        send_hello_thread.start()
+        # connect to server
+        result = self.connect_to_server()
+        if isinstance(result, str):
+            # unsuccessful connection
+            return result
 
-        # start receive thread
-        receive_thread = threading.Thread(target=self.receive_data)
-        receive_thread.start()
+        elif result is True:
+            # peer discovery
+            send_hello_thread = threading.Thread(target=self.send_hello, args=(True, ))
+            send_hello_thread.start()
 
-        # start cleanup thread
-        cleanup_thread = threading.Thread(target=self.cleanup)
-        cleanup_thread.daemon = True
-        cleanup_thread.start()
+            # start receive thread
+            receive_thread = threading.Thread(target=self.receive_data)
+            receive_thread.start()
 
-        # start mining thread
-        miner_thread = threading.Thread(target=self.miner_thread)
-        miner_thread.daemon = True
-        miner_thread.start()
+            # start cleanup thread
+            cleanup_thread = threading.Thread(target=self.cleanup)
+            cleanup_thread.daemon = True
+            cleanup_thread.start()
+
+            # start mining thread
+            miner_thread = threading.Thread(target=self.miner_thread)
+            miner_thread.daemon = True
+            miner_thread.start()
+
+            return True
 
 
